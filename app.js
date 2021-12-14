e2bbfc53ee837b0ba742ec763e69f7e42d0df396
apple <samuelyang5210@gmail.com>
2021-12-14 20:05:39 +0800

    Set up passport

--- app.js
+++ app.js
@@ -4,42 +4,75 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema ({
-    email: {type: String, required: [true, "No email specified"]},
-    password: {type: String, required: [true, "No password specified"]},
+    email: {type: String},
+    password: {type: String},
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.route("/")
     .get(function(request, response){
         response.render("home");
     });
 
+app.get("/secrets", function(request, response){
+        if(request.isAuthenticated()){
+            response.render("secrets");
+        }
+        else{
+            response.redirect("/login");
+        }
+});
+
 app.route("/register")
     .get(function(request, response){
         response.render("register");
     })
     .post(async function(request, response){
-        let user = new User ({
-            email: request.body.username,
-            password: request.body.password,
-        });
-        await user.save(function(error){
-            if(error){response.send(error);}
-            else{response.render("secrets");}
-        })
+        User.register(
+            {username: request.body.username}, 
+            request.body.password,
+            function(error, user){
+                if(error){
+                    console.log(error);
+                    response.redirect("/register");
+                }
+                else{
+                    passport.authenticate("local")(request, response, function(){
+                        response.redirect("/secrets");
+                    });
+                }
+            }
+        );
     });
 
 app.route("/login")
@@ -47,22 +80,7 @@ app.route("/login")
         response.render("login");
     })
     .post(async function(request, response){
-        User.findOne(
-        {
-            email: request.body.username,
-            password: request.body.password,
-        }, function(error, user){
-            if(error){response.send(error);}
-            else{
-                if(user){
-                    response.render("secrets");
-                }
-                else{
-                    response.send("<h1>Cannot find user with corresponding\
-                        username and password.</h1>");
-                    }
-            }
-        });
+        
     });
 
 app.listen(3000);
