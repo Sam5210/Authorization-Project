013433d8179d020f32ddc1294e54ad81736a24b3
apple <samuelyang5210@gmail.com>
2021-12-17 15:19:53 +0800

    Complete project v1.0

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 //jshint esversion:6
 require("dotenv").config();
+const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
-  saveUninitialized: false
-}))
+  saveUninitialized: false,
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -26,33 +27,95 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema ({
-    email: {type: String},
-    password: {type: String},
+    email: String,
+    username: String,
+    password: String,
+    googleId: String,
+    secrets: [String]
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+      done(err, user);
+    });
+});
 
-app.route("/")
-    .get(function(request, response){
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile.id);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/", function(request, response){
         response.render("home");
     });
 
 app.get("/secrets", function(request, response){
-        if(request.isAuthenticated()){
-            response.render("secrets");
+    User.find(
+        {secrets: {$exists : true, $not: {$size: 0}}},
+        function(error, foundUsers){
+            if(error){
+                console.log(error);
+            }
+            else{
+                if(foundUsers){
+                    response.render("secrets", {usersWithSecrets: foundUsers});
+                }
+            }
         }
-        else{
-            response.redirect("/login");
-        }
+    );
 });
 
+app.get("/submit", function(request, response){
+    if(request.isAuthenticated()){
+        response.render("submit");
+    }
+    else{
+        response.redirect("/login");
+    }
+});
+app.post("/submit", function(request, response){
+    const secret = request.body.secret;
+    User.findById(request.user.id, function(error, foundUser){
+        if(error){console.log(error);}
+        else{
+            if(foundUser){
+                foundUser.secrets.push(secret);
+                foundUser.save(function(){
+                    response.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login"}),
+  function(request, response) {
+    // Successful authentication, redirect secrets.
+    response.redirect("/secrets");
+});
 app.route("/register")
     .get(function(request, response){
         response.render("register");
@@ -108,10 +171,9 @@ app.route("/login")
         });
     });
 
-app.route("/logout")
-    .get(function(request, response){
+app.get("/logout", function(request, response){
         request.logout();
         response.redirect("/");
-    });
+});
 
 app.listen(3000);
